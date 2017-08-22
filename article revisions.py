5540f3bae3e0c915d1940a6a0010852dd1f5818e
RiceChrispies <ricechrispies@gmail.com>
2017-08-22 17:08:44 +0200

    made some progress on the login. Now it's printing the soup, but still not actually logging in

--- article revisions.py
+++ article revisions.py
@@ -1,10 +1,17 @@
 import bs4 as bs
 import urllib.request
 import fileinput
-from simple_salesforce import salesforce
-#Issues calling this, but need to table this for now.
+from simple_salesforce import Salesforce
+import requests
+# https://stackoverflow.com/questions/11892729/how-to-log-in-to-a-website-using-pythons-requests-module
+session = requests.Session()
+sf = Salesforce(instance='***', username='***', password='***', security_token='***')
 
-sf-Salesforce(username='***', password='***', security_token='***')
+#session_id, instance = SalesforceLogin(
+#    username='***',
+#    password='***',
+#    security_token='***',
+#    sandbox=False)
 
 source = urllib.request.urlopen('https://eu1.salesforce.com/kA5D00000004MIb').read()
 soup = bs.BeautifulSoup(source,'lxml')
